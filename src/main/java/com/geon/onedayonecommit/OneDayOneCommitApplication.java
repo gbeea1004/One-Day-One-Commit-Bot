9533461f647e41f4a3a36ae8d1bf25ffb20be304
geonHee <corqkrtk3@naver.com>
2019-12-21 18:29:54 +0900

    Init project

---
+++ src/main/java/com/geon/onedayonecommit/OneDayOneCommitApplication.java
@@ -0,0 +1,13 @@
+package com.geon.onedayonecommit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDayOneCommitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneDayOneCommitApplication.class, args);
+	}
+
+}
